cc3192626bb8daccd7e0b501574d504a89611252
VidnyaniU <vidnyani.umathe@gmail.com>
2023-11-25 23:45:18 +0530

    updated precision and added binary

--- precision.c
+++ precision.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 // Write a computer program and run it several times , with varying inputs , to find out the largest and smallest
 // integers that can be stored in a given computer
 
@@ -10,13 +9,19 @@ int limitOfInt(int n)
 }
 
 // find out largest  and smallest powers of 10 of a single precision floating point that can be stored in a given computer
-int limitOfFloat(){
-    
+int limitOfFloat(float m)
+{
+    return 0;
 }
 int main()
 {
     int n = 32;
-    printf("%d", limitOfInt(n));
+
+    float m = 6581.845f;
+
+    // printf("%d", limitOfInt(n));
+
+    printf("%f", m);
 
     return 0;
 }
